ef8ce5d931a4b497776f8b0547e87ea7856e22c4
Admin <Admin@ADMIN>
2023-10-29 22:08:23 +0700

    Add project files.

---
+++ baitapLab8.3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+class Product
+{
+    public int id { get; set; }
+    public string name { get; set; }
+    public int price { get; set;}
+    public Product() { }
+    public Product(int id, string name, int price)
+    {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+    public override string ToString()
+    {
+        return $"Id: {id}, Name: {name}, Price: {price}";
+    }
+}
+class ProductEnumberable: Product, IEnumerable
+{
+    List<Product> products = new List<Product>()
+    {
+        new Product{id=1,name="Ban",price=500000 },
+        new Product(2,"Ghe",100000),
+        
+    };
+    public IEnumerator GetEnumerator()
+    {
+        for(int i=0; i < products.Count; i++)
+        {
+            yield return products[i];
+        }
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        ProductEnumberable products = new ProductEnumberable();
+        foreach(var item in products)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
---
+++ baitapLab8/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Generic<T>
+{
+    T[] a;
+    int count;
+    public Generic(int n)
+    {
+        a = new T[n];
+        count = 0;
+    }
+    public void Add(T value)
+    {
+        if (count < a.Length)
+        {
+            a[count] = value;
+            count++;
+        }
+
+    }
+    public void Display()
+    {
+        Console.Write("Mang gom cac phan tu: ");
+        foreach(var i in a)
+        {
+            Console.Write(i+" ");
+        }
+        Console.WriteLine();
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Nhap so phan tu cho mang: ");
+        int n = int.Parse(Console.ReadLine());
+        Generic<int> a = new Generic<int>(n);
+        Console.WriteLine("Nhap cac phan tu so nguyen cho mang: ");
+        for(int i = 0; i < n; i++)
+        {
+            int x = int.Parse(Console.ReadLine());
+            a.Add(x);
+        }
+        a.Display();
+        Console.ReadKey();
+
+        Generic<char> b = new Generic<char>(n);
+        Console.WriteLine("Nhap cac phan tu ky tu cho mang: ");
+        for(int i = 0; i < n; i++)
+        {
+            char x = char.Parse(Console.ReadLine());
+            b.Add(x);
+        }
+        b.Display();
+    }
+}
