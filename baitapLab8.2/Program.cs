96da791f77c25b2508f357886f1810a3452dbbde
MaiTheLongDraco <128013309+MaiTheLongDraco@users.noreply.github.com>
2023-10-31 18:28:28 +0700

    edit

---
+++ baitapLab8.2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        void Swap<T>(ref T a, ref T b)
+        {
+            T temp = a;
+            a = b;
+            b = temp;
+            Console.WriteLine("Sau khi swap: " + a + ", " + b);
+        }
+        Console.WriteLine("Nhap 2 phan tu a va b: ");
+        int a = 6, b = 3;
+        Swap<int>(ref a, ref b);
+        char x = 'A', y = 'B';
+        Swap<char>(ref x, ref y);
+        string i = "abc", j = "xyz";
+        Swap<string>(ref i, ref j);
+    }
+}
